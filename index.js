6c625ce1f30949dc3346a3a83d1c739864bb550c
Ville Vaarala <ville.l.vaarala@gmail.com>
2018-02-08 16:31:32 +0200

    Enhance missing tokens detection

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ require('dotenv').config();
 const auth = require('./modules/auth');
 const callAppsScript = require('./modules/callAppsScript');
 
+function isNonEmptyString(maybeString) {
+  return typeof maybeString === 'string' && maybeString.length > 0;
+}
+
 const hasTokens =
-  typeof process.env.ACCESS_TOKEN !== 'undefined' &&
-  typeof process.env.REFRESH_TOKEN !== 'undefined';
+  isNonEmptyString(process.env.ACCESS_TOKEN) &&
+  isNonEmptyString(process.env.REFRESH_TOKEN);
 
 if (hasTokens) {
   auth.setCredentials({
     token_type: 'Bearer',
     access_token: process.env.ACCESS_TOKEN,
-    refresh_token: process.env.REFRESH_TOKEN,
+    refresh_token: process.env.REFRESH_TOKEN
   });
   callAppsScript(auth, function(resp) {
     console.log(resp.data);
   });
 } else {
   console.log(`
-Please get authorization tokens by running \`yarn auth\`
+Google OAuth2 tokens are not defined.
+Please get authorization tokens by running \`yarn auth\`.
   `);
   process.exit(1);
 }
